4f446b76332182f62af5ef4d26e3af4fd09d19fe
Kai Pridham <kai@pridham.net.au>
2025-08-30 06:34:44 +0000

    fixup! feat: add VM startup script

--- vm_startup.sh
+++ vm_startup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -eux -o pipefail
 
+# install configures OS packages and configures certbot and nginx
 install() {
   sudo apt-get update
 
@@ -12,9 +13,18 @@ install() {
   sudo ln -s /snap/bin/certbot /usr/bin/certbot
 
   # TODO: Override default nginx site.
+  # TODO: Create and symlink forge site.
 
+  # TODO: Get certbot to update forge site.
   sudo certbot run --noninteractive --agree-tos \
     --nginx --domain forge.priddles.xyz
+
+  sudo systemctl start nginx
+}
+
+# prep_data formats the data disk for use.
+prep_data() {
+  -E lazy_itable_init=0,lazy_journal_init=0,discard
 }
 
 # TODO: Only run install once.
