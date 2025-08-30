1cf4ab02d257e924996566d8e910d515a67153f3
Kai Pridham <kai@pridham.net.au>
2025-08-30 06:34:44 +0000

    fixup! feat: add VM startup script

--- vm_startup.sh
+++ vm_startup.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 set -eux -o pipefail
+
+sudo apt-get update
+sudo apt-get install -y nginx snapd
+
+sudo snap install --classic certbot
+sudo ln -s /snap/bin/certbot /usr/bin/certbot
+
+# TODO: Update A record in DNS.
+
+sudo certbot run --noninteractive --agree-tos \
+  --nginx \
+  --domain forge.priddles.xyz
