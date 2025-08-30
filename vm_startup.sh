10a3f08e189f995ce7b4992ed3d97efab534232f
Kai Pridham <kai@pridham.net.au>
2025-08-30 06:34:44 +0000

    fixup! feat: add VM startup script

--- vm_startup.sh
+++ vm_startup.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 set -eux -o pipefail
 
-sudo apt-get update
-sudo apt-get install -y nginx snapd
+install() {
+  sudo apt-get update
 
-sudo snap install --classic certbot
-sudo ln -s /snap/bin/certbot /usr/bin/certbot
+  sudo apt-get install -y nginx
+  sudo systemctl stop nginx
+
+  sudo apt-get install -y snapd
+  sudo snap install --classic certbot
+  sudo ln -s /snap/bin/certbot /usr/bin/certbot
+
+  # TODO: Override default nginx site.
+
+  sudo certbot run --noninteractive --agree-tos \
+    --nginx --domain forge.priddles.xyz
+}
+
+# TODO: Only run install once.
+install
+
+# TODO: Write nginx sites to: /etc/nginx/sites-available
+# TODO: Create symlinks in: /etc/nginx/sites-enabled
 
 # TODO: Update A record in DNS.
-
-sudo certbot run --noninteractive --agree-tos \
-  --nginx \
-  --domain forge.priddles.xyz
